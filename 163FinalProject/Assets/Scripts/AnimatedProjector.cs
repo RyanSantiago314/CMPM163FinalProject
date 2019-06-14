a6decf029cb13a67fdccf53378ae5b83c79c0a89
Yash Dua <ydua@ucsc.edu>
2019-06-14 03:18:55 -0800

    added caustics

---
+++ 163FinalProject/Assets/Scripts/AnimatedProjector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedProjector : MonoBehaviour
+{
+    public float fps = 30.0f;
+    public Texture2D[] frames;
+
+    private int frameIndex;
+    private Projector projector;
+
+    void Start()
+    {
+        projector = GetComponent<Projector>();
+        NextFrame();
+        InvokeRepeating("NextFrame", 1 / fps, 1 / fps);
+        Debug.Log("gets here");
+    }
+
+    void NextFrame()
+    {
+        projector.material.SetTexture("_ShadowTex", frames[frameIndex]);
+        frameIndex = (frameIndex + 1) % frames.Length;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.C))
+        {
+            projector.enabled = !projector.enabled;
+        }
+    }
+}
