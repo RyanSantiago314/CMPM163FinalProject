54519bb300de51dda92c39c127a1a2b2702d1a4f
Ryan Santiago <ryansantiago.d7s@gmail.com>
2019-06-14 01:31:59 -0700

    Made some adjustments to sliding

--- 163FinalProject/Assets/Scripts/Slide.cs
+++ 163FinalProject/Assets/Scripts/Slide.cs
@@ -18,15 +18,15 @@ public class Slide : MonoBehaviour
         if (timer < 1)
         {
             if (timer < 1 && timer < .5)
-                timer += .05f;
-            else if (timer < 1 && timer < .75)
                 timer += .02f;
-            else if (timer < 1 && timer < .87)
+            else if (timer < 1 && timer < .75)
                 timer += .01f;
-            else if (timer < 1)
+            else if (timer < 1 && timer < .87)
                 timer += .005f;
+            else if (timer < 1)
+                timer += .002f;
             transform.position = Vector3.Lerp(new Vector3(-9f, -9.06f, -.03f), new Vector3(10f, -9.06f, -.03f), timer);
-            rock -= .27f;
+            rock -= .15f;
             Quaternion newRot = new Quaternion();
             newRot.eulerAngles = new Vector3(0, 0, rock);
             transform.rotation = newRot;
